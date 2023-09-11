88e96954d6e5ee86fa03ca12c045f53832721879
Valerjan-1 <134737688+Valerjan-1@users.noreply.github.com>
2023-09-11 20:26:29 +0300

    summary

---
+++ js/script-hw-6.js
@@ -0,0 +1,17 @@
+const numberOfCategories = document.querySelector("#categories");
+const categories = [...numberOfCategories.children];
+console.log("Number of categories:", categories.length);
+
+categories.forEach(function (category) {
+  const categorie = category.children;
+  console.log("Category:", categorie[0].textContent);
+  console.log("Elements:", categorie[1].children.length);
+});
+
+// console.log(
+//     "Category:",
+//     categorie[0].textContent,
+//     "Elements:",
+//     categorie[1].children.length
+//   );
+// });
